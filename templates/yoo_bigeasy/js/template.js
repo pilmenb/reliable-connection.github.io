e64096a540524d2ca1333c710a4d1573cd928776
pilmenb <38426140+pilmenb@users.noreply.github.com>
2018-04-16 14:43:55 +0300

    master

---
+++ templates/yoo_bigeasy/js/template.js
@@ -0,0 +1,47 @@
+/* Copyright (C) YOOtheme GmbH, YOOtheme Proprietary Use License (http://www.yootheme.com/license) */
+
+(function($){
+
+	$(document).bind('ready', function() {
+
+		var config = $('body').data('config') || {};
+
+		// Accordion menu
+		$('.menu-sidebar').accordionMenu({ mode:'slide' });
+
+
+		// Smoothscroller
+		$('a[href="#page"]').smoothScroller({ duration: 500 });
+
+		// Fix Browser Rounding
+		$('.grid-block').matchWidth('.grid-h');
+
+		// Social buttons
+		$('article[data-permalink]').socialButtons(config);
+
+		// Match height of div tags
+		var matchHeight = function(){
+			$('#top-a .grid-h').matchHeight('.deepest');
+			$('#top-b .grid-h').matchHeight('.deepest');
+			$('#bottom-a .grid-h').matchHeight('.deepest');
+			$('#bottom-b .grid-h').matchHeight('.deepest');
+			$('#innertop .grid-h').matchHeight('.deepest');
+			$('#innerbottom .grid-h').matchHeight('.deepest');
+			$('#maininner, #sidebar-a, #sidebar-b').matchHeight();
+			$('.wrapper').css("min-height", $(window).height());
+		};
+
+		matchHeight();
+
+		$(window).bind("load", function(){
+			
+			// Dropdown menu
+			$('#menu').dropdownMenu({ mode: 'slide', dropdownSelector: 'div.dropdown'});
+			
+			matchHeight();	
+
+		});
+		
+	});
+	
+})(jQuery);
